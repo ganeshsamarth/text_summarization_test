c6b9fbcabd52cdc913f92da5dd1e17e8636fb568
ganeshsamarth <47754795+ganeshsamarth@users.noreply.github.com>
2019-07-07 02:27:15 +0530

    build_model.py contains the model in tensorflow

---
+++ build_model.py
@@ -0,0 +1,48 @@
+#building the model in tensorflow
+from data_clean.py import *
+def model_inputs():
+    '''Create palceholders for inputs to the model'''
+
+    input_data = tf.placeholder(tf.int32, [None, None], name='input')
+    targets = tf.placeholder(tf.int32, [None, None], name='targets')
+    lr = tf.placeholder(tf.float32, name='learning_rate')
+    keep_prob = tf.placeholder(tf.float32, name='keep_prob')
+    summary_length = tf.placeholder(tf.int32, (None,), name='summary_length')
+    max_summary_length = tf.reduce_max(summary_length, name='max_dec_len')
+    text_length = tf.placeholder(tf.int32, (None,), name='text_length')
+
+    return input_data, targets, lr, keep_prob, summary_length, max_summary_length, text_length
+
+
+def process_encoding_input(target_data, vocab_to_int, batch_size):
+    '''Remove the last word id from each batch and concat the <GO> to the begining of each batch'''
+
+    ending = tf.strided_slice(target_data, [0, 0], [batch_size, -1], [1, 1])
+    dec_input = tf.concat([tf.fill([batch_size, 1], vocab_to_int['<GO>']), ending], 1)
+
+    return dec_input
+
+def encoding_layer(rnn_size, sequence_length, num_layers, rnn_inputs, keep_prob):
+    '''Create the encoding layer'''
+
+    for layer in range(num_layers):
+        with tf.variable_scope('encoder_{}'.format(layer)):
+            cell_fw = tf.contrib.rnn.LSTMCell(rnn_size,
+                                              initializer=tf.random_uniform_initializer(-0.1, 0.1, seed=2))
+            cell_fw = tf.contrib.rnn.DropoutWrapper(cell_fw,
+                                                    input_keep_prob = keep_prob)
+
+            cell_bw = tf.contrib.rnn.LSTMCell(rnn_size,
+                                              initializer=tf.random_uniform_initializer(-0.1, 0.1, seed=2))
+            cell_bw = tf.contrib.rnn.DropoutWrapper(cell_bw,
+                                                    input_keep_prob = keep_prob)
+
+            enc_output, enc_state = tf.nn.bidirectional_dynamic_rnn(cell_fw,
+                                                                    cell_bw,
+                                                                    rnn_inputs,
+                                                                    sequence_length,
+                                                                    dtype=tf.float32)
+    # Join outputs since we are using a bidirectional RNN
+    enc_output = tf.concat(enc_output,2)
+
+    return enc_output, enc_state
