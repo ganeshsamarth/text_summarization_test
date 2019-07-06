4ec688515abe43dbb40ff027dd1365ec2c9d4576
ganesh <ganesam8804@gmail.com>
2019-07-06 11:55:10 +0100

    lstm.py

---
+++ lstm_Attention.py
@@ -0,0 +1,205 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr  3 09:31:45 2018
+
+@author: moseli
+"""
+from numpy.random import seed
+seed(1)
+
+from sklearn.model_selection import train_test_split as tts
+import logging
+
+from pyrouge import Rouge155 
+import matplotlib.pyplot as plt
+import keras
+from keras import backend as k
+k.set_learning_phase(1)
+from keras import initializers
+from keras.optimizers import RMSprop
+from keras.models import Model
+from keras.layers import Dense,LSTM,Input,Activation,Add,TimeDistributed,\
+Permute,Flatten,RepeatVector,merge,Lambda,Multiply,Reshape
+from keras.callbacks import ModelCheckpoint
+from keras.models import load_model
+import tensorflow as tf
+
+from word2vec import *
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s',\
+    level=logging.INFO)
+
+#######################model params###########################
+batch_size = 50
+num_classes = 1
+epochs = 20
+hidden_units = emb_size_all
+learning_rate = 0.005
+clip_norm = 2.0
+
+en_shape=np.shape(train_data["article"][0])
+de_shape=np.shape(train_data["summaries"][0])
+
+#######################################################################
+############################Helper Functions###########################
+#######################################################################    
+
+def encoder_decoder(data):
+    print('Encoder_Decoder LSTM...')
+   
+    """__encoder___"""
+    encoder_inputs = Input(shape=en_shape)
+    
+    encoder_LSTM = LSTM(hidden_units,dropout_U=0.2,dropout_W=0.2,return_sequences=True,return_state=True)
+    encoder_LSTM_rev=LSTM(hidden_units,return_state=True,return_sequences=True,dropout_U=0.05,dropout_W=0.05,go_backwards=True)
+    
+    encoder_outputs, state_h, state_c = encoder_LSTM(encoder_inputs)
+    encoder_outputsR, state_hR, state_cR = encoder_LSTM_rev(encoder_inputs)
+    
+    state_hfinal=Add()([state_h,state_hR])
+    state_cfinal=Add()([state_c,state_cR])
+    encoder_outputs_final = Add()([encoder_outputs,encoder_outputsR])
+    
+    encoder_states = [state_hfinal,state_cfinal]
+    
+    """____decoder___"""
+    decoder_inputs = Input(shape=(None,de_shape[1]))
+    decoder_LSTM = LSTM(hidden_units,return_sequences=True,dropout_U=0.2,dropout_W=0.2,return_state=True)
+    decoder_outputs, _, _ = decoder_LSTM(decoder_inputs,initial_state=encoder_states)
+    
+    #Pull out XGBoost, (I mean attention)
+    attention = TimeDistributed(Dense(1, activation = 'tanh'))(encoder_outputs_final)
+    attention = Flatten()(attention)
+    attention = Multiply()([decoder_outputs, attention])
+    attention = Activation('softmax')(attention)
+    attention = Permute([2, 1])(attention)
+    print(tf.shape(attention))
+ 
+    decoder_dense = Dense(de_shape[1],activation='softmax')
+    decoder_outputs = decoder_dense(attention)
+    
+    model= Model(inputs=[encoder_inputs,decoder_inputs], outputs=decoder_outputs)
+    print(model.summary())
+    
+    rmsprop = RMSprop(lr=learning_rate,clipnorm=clip_norm)
+    model.compile(loss='categorical_crossentropy',optimizer=rmsprop,metrics=['accuracy'])
+    
+    x_train,x_test,y_train,y_test=tts(data["article"],data["summaries"],test_size=0.20)
+    history= model.fit(x=[x_train,y_train],
+              y=y_train,
+              batch_size=batch_size,
+              epochs=epochs,
+              verbose=1,
+              validation_data=([x_test,y_test], y_test))
+    print(model.summary())
+    """_________________inference mode__________________"""
+    encoder_model_inf = Model(encoder_inputs,encoder_states)
+    
+    decoder_state_input_H = Input(shape=(en_shape[0],))
+    decoder_state_input_C = Input(shape=(en_shape[0],)) 
+    decoder_state_inputs = [decoder_state_input_H, decoder_state_input_C]
+    decoder_outputs, decoder_state_h, decoder_state_c = decoder_LSTM(decoder_inputs,
+                                                                     initial_state=decoder_state_inputs)
+    decoder_states = [decoder_state_h, decoder_state_c]
+    decoder_outputs = decoder_dense(decoder_outputs)
+    
+    decoder_model_inf= Model([decoder_inputs,decoder_state_inputs],
+                         [decoder_outputs,decoder_states])
+    
+    scores = model.evaluate([x_test,y_test],y_test, verbose=1)
+    
+    
+    print('LSTM test scores:', scores)
+    print('\007')
+    print(model.summary())
+    return model,encoder_model_inf,decoder_model_inf,history
+
+
+"""_________generate summary from vectors_____________"""
+
+
+def generateText(SentOfVecs):
+    SentOfVecs=np.reshape(SentOfVecs,de_shape)
+    kk=""
+    for k in SentOfVecs:
+        kk = kk + label_encoder.inverse_transform([argmax(k)])[0].strip()+" "
+        #kk=kk+((getWord(k)[0]+" ") if getWord(k)[1]>0.01 else "")
+    return kk
+
+"""________________generate summary vectors___________"""
+
+def summarize(article):
+    stop_pred = False
+    article =  np.reshape(article,(1,en_shape[0],en_shape[1]))
+    #get initial h and c values from encoder
+    init_state_val = encoder.predict(article)
+    target_seq = np.zeros((1,1,de_shape[1]))
+    #target_seq =np.reshape(train_data['summaries'][k][0],(1,1,de_shape[1]))
+    generated_summary=[]
+    while not stop_pred:
+        decoder_out,decoder_h,decoder_c= decoder.predict(x=[target_seq,init_state_val])
+        generated_summary.append(decoder_out)
+        init_state_val= [decoder_h,decoder_c]
+        #get most similar word and put in line to be input in next timestep
+        #target_seq=np.reshape(model.wv[getWord(decoder_out)[0]],(1,1,emb_size_all))
+        target_seq=np.reshape(decoder_out,(1,1,de_shape[1]))
+        if len(generated_summary)== de_shape[0]:
+            stop_pred=True
+
+    return generated_summary
+
+"""__________________Plot training curves_______________"""
+
+def plot_training(history):
+    print(history.history.keys())
+    #  "Accuracy"
+    plt.plot(history.history['acc'])
+    plt.plot(history.history['val_acc'])
+    plt.title('model accuracy')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'validation'], loc='upper left')
+    plt.show()
+    # "Loss"
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('model loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'validation'], loc='upper left')
+    plt.show()
+
+
+def saveModels():
+    trained_model.save("%sinit_model"%modelLocation)
+    encoder.save("%sencoder"%modelLocation)
+    decoder.save("%sdecoder"%modelLocation)
+    
+def evaluate_summ(article):
+    ref=''
+    for k in wt(data['summaries'][article])[:20]:
+        ref=ref+' '+k
+    gen_sum = generateText(summarize(train_data["article"][article]))
+    print("-----------------------------------------------------")
+    print("Original summary")
+    print(ref)
+    print("-----------------------------------------------------")
+    print("Generated summary")
+    print(gen_sum)
+    print("-----------------------------------------------------")
+    rouge = Rouge155()
+    score = rouge.score_summary(ref, gen_sum)
+    print("Rouge1 Score: ",score)
+        
+#######################################################################################
+################################ Train model and test##################################
+#######################################################################################
+
+
+trained_model,encoder,decoder,history = encoder_decoder(train_data)
+plot_training(history)
+evaluate_summ(10)
+
+print(generateText(summarize(train_data["article"][8])))
+print(data["summaries"][8])
+print(data["articles"][8])
+
---
+++ word2vec.py
@@ -0,0 +1,222 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 28 21:59:33 2018
+
+@author: moseli
+"""
+from numpy.random import seed
+seed(1)
+
+modelLocation="/home/pbu/Bidirectiona-LSTM-for-text-summarization-/TrainedModels"
+
+
+import gensim as gs
+import pandas as pd
+import numpy as np
+import scipy as sc
+import nltk
+nltk.download('punkt')
+from nltk.tokenize import word_tokenize as wt
+from nltk.tokenize import sent_tokenize as st
+from numpy import argmax
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import OneHotEncoder
+from keras.preprocessing.sequence import pad_sequences
+import logging
+import re
+from collections import Counter
+
+from cnn_daily_load import *
+
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s',\
+    level=logging.INFO)
+
+########################################################################
+############################helpers####################################
+#######################################################################
+emb_size_all = 300
+maxcorp=5000
+
+
+def createCorpus(t):
+    corpus = []
+    all_sent = []
+    for k in t:
+        for p in t[k]:
+            corpus.append(st(p))
+    for sent in range(len(corpus)):
+        for k in corpus[sent]:
+            all_sent.append(k)
+    for m in range(len(all_sent)):
+        all_sent[m] = wt(all_sent[m])
+    
+    all_words=[]
+    for sent in all_sent:
+        hold=[]
+        for word in sent:
+            hold.append(word.lower())
+        all_words.append(hold)
+    return all_words
+
+
+def word2vecmodel(corpus):
+    emb_size = emb_size_all
+    model_type={"skip_gram":1,"CBOW":0}
+    window=10
+    workers=4
+    min_count=4
+    batch_words=20
+    epochs=25
+    #include bigrams
+    #bigramer = gs.models.Phrases(corpus)
+
+    model=gs.models.Word2Vec(corpus,size=emb_size,sg=model_type["skip_gram"],
+                             compute_loss=True,window=window,min_count=min_count,workers=workers,
+                             batch_words=batch_words)
+        
+    model.train(corpus,total_examples=len(corpus),epochs=epochs)
+    model.save("%sWord2vec"%modelLocation)
+    print('\007')
+    return model
+
+
+def summonehot(corpus):
+    allwords=[]
+    annotated={}
+    for sent in corpus:
+        for word in wt(sent):
+            allwords.append(word.lower())
+    print(len(set(allwords)), "unique characters in corpus")
+    #maxcorp=int(input("Enter desired number of vocabulary: "))
+    maxcorp=int(len(set(allwords))/1.1)
+    wordcount = Counter(allwords).most_common(maxcorp)
+    allwords=[]
+    
+    for p in wordcount:
+        print(p[0])
+        allwords.append(p[0])  
+        
+    allwords=list(set(allwords))
+    
+    print(len(allwords), "unique characters in corpus after max corpus cut")
+    #integer encode
+    label_encoder = LabelEncoder()
+    integer_encoded = label_encoder.fit_transform(allwords)
+    #one hot
+    onehot_encoder = OneHotEncoder(sparse=False)
+    integer_encoded = integer_encoded.reshape(len(integer_encoded), 1)
+    onehot_encoded = onehot_encoder.fit_transform(integer_encoded)
+    #make look up dict
+    for k in range(len(onehot_encoded)): 
+        inverted = cleantext(label_encoder.inverse_transform([argmax(onehot_encoded[k, :])])[0]).strip()
+        annotated[inverted]=onehot_encoded[k]
+    return label_encoder,onehot_encoded,annotated
+
+
+
+def wordvecmatrix(model,data):
+    IO_data={"article":[],"summaries":[]}
+    i=1
+    for k in range(len(data["articles"])):
+        art=[]
+        summ=[]
+        for word in wt(data["articles"][k].lower()):
+            try:
+                art.append(model.wv.word_vec(word))
+            except Exception as e:
+                print(e)
+
+        for word in wt(data["summaries"][k].lower()):
+            try:
+                summ.append(onehot[word])
+                #summ.append(model.wv.word_vec(word))
+            except Exception as e:
+                print(e)
+        
+        IO_data["article"].append(art) 
+        IO_data["summaries"].append(summ)
+        if i%100==0:
+            print("progress: " + str(((i*100)/len(data["articles"]))))
+        i+=1
+    #announcedone()
+    print('\007')
+    return IO_data
+
+def cutoffSequences(data,artLen,sumlen):
+    data2={"article":[],"summaries":[]}
+    for k in range(len(data["article"])):
+        if len(data["article"][k])<artLen or len(data["summaries"][k])<sumlen:
+             #data["article"]=np.delete(data["article"],k,0)
+             #data["article"]=np.delete(data["summaries"],k,0)
+             pass
+        else:
+            data2["article"].append(data["article"][k][:artLen])
+            data2["summaries"].append(data["summaries"][k][:sumlen])
+    return data2
+
+
+def max_len(data):
+    lenk=[]
+    for k in data:
+        lenk.append(len(k))
+    print("The minimum length is: ",min(lenk))
+    print("The average length is: ",np.average(lenk))
+    print("The max length is: ",max(lenk))
+    return min(lenk),max(lenk)
+
+"""reshape vectres for Gensim"""
+def reshape(vec):
+    return np.reshape(vec,(1,emb_size_all))
+
+def addones(seq):
+    return np.insert(seq, [0], [[0],], axis = 0)
+
+def endseq(seq):
+    pp=len(seq)
+    return np.insert(seq, [pp], [[1],], axis = 0)
+#######################################################################
+#######################################################################
+
+corpus = createCorpus(data)
+
+label_encoder,onehot_encoded,onehot=summonehot(data["summaries"])
+
+model=word2vecmodel(corpus)
+
+model.get_latest_training_loss()
+
+train_data = wordvecmatrix(model,data)
+
+print(len(train_data["article"]), "training articles")
+
+train_data=cutoffSequences(train_data,300,25)
+
+#seq length stats
+max_len(train_data["article"])
+max_len(train_data["summaries"])
+
+
+train_data["summaries"]=np.array(train_data["summaries"])
+print(train_data['summaries'][0].shape)
+train_data["article"]=np.array(train_data["article"])
+print(train_data['article'][0].shape)
+
+
+#add end sequence for each article
+
+#train_data["summaries"]=np.array(list(map(endseq,train_data["summaries"])))
+#train_data["article"]=np.array(list(map(endseq,train_data["article"])))
+
+print("summary length: ",len(train_data["summaries"][0]))
+print("article length: ",len(train_data["article"][0]))
+
+
+"""__pad sequences__
+train_data["article"]=pad_sequences(train_data["article"],maxlen=max_len(train_data["article"]),
+          padding='post',dtype=float)
+train_data["summaries"]=pad_sequences(train_data["summaries"],maxlen=max_len(train_data["summaries"]),
+          padding='post',dtype=float)
+"""
+#add start sequence
+train_data["summaries"]=np.array(list(map(addones,train_data["summaries"])))
+
