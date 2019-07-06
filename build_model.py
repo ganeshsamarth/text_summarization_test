2f57697795f32c7ca33b38312c8aaf3f513dd0bf
ganeshsamarth <47754795+ganeshsamarth@users.noreply.github.com>
2019-07-07 02:31:49 +0530

    Update build_model.py

--- build_model.py
+++ build_model.py
@@ -1,5 +1,7 @@
 #building the model in tensorflow
-from data_clean.py import *
+#from data_clean.py import *
+import tensorflow as tf
+
 def model_inputs():
     '''Create palceholders for inputs to the model'''
 
@@ -46,3 +48,144 @@ def encoding_layer(rnn_size, sequence_length, num_layers, rnn_inputs, keep_prob)
     enc_output = tf.concat(enc_output,2)
 
     return enc_output, enc_state
+
+def training_decoding_layer(dec_embed_input, summary_length, dec_cell, initial_state, output_layer,
+                            vocab_size, max_summary_length):
+    '''Create the training logits'''
+
+    training_helper = tf.contrib.seq2seq.TrainingHelper(inputs=dec_embed_input,
+                                                        sequence_length=summary_length,
+                                                        time_major=False)
+
+    training_decoder = tf.contrib.seq2seq.BasicDecoder(dec_cell,
+                                                       training_helper,
+                                                       initial_state,
+                                                       output_layer)
+
+    training_logits, _ = tf.contrib.seq2seq.dynamic_decode(training_decoder,
+                                                           output_time_major=False,
+                                                           impute_finished=True,
+                                                           maximum_iterations=max_summary_length)
+    return training_logits
+def inference_decoding_layer(embeddings, start_token, end_token, dec_cell, initial_state, output_layer,
+                             max_summary_length, batch_size):
+    '''Create the inference logits'''
+
+    start_tokens = tf.tile(tf.constant([start_token], dtype=tf.int32), [batch_size], name='start_tokens')
+
+    inference_helper = tf.contrib.seq2seq.GreedyEmbeddingHelper(embeddings,
+                                                                start_tokens,
+                                                                end_token)
+
+    inference_decoder = tf.contrib.seq2seq.BasicDecoder(dec_cell,
+                                                        inference_helper,
+                                                        initial_state,
+                                                        output_layer)
+
+    inference_logits, _ = tf.contrib.seq2seq.dynamic_decode(inference_decoder,
+                                                            output_time_major=False,
+                                                            impute_finished=True,
+                                                            maximum_iterations=max_summary_length)
+
+    return inference_logits
+def decoding_layer(dec_embed_input, embeddings, enc_output, enc_state, vocab_size, text_length, summary_length,
+                   max_summary_length, rnn_size, vocab_to_int, keep_prob, batch_size, num_layers):
+    '''Create the decoding cell and attention for the training and inference decoding layers'''
+
+    for layer in range(num_layers):
+        with tf.variable_scope('decoder_{}'.format(layer)):
+            lstm = tf.contrib.rnn.LSTMCell(rnn_size,
+                                           initializer=tf.random_uniform_initializer(-0.1, 0.1, seed=2))
+            dec_cell = tf.contrib.rnn.DropoutWrapper(lstm,
+                                                     input_keep_prob = keep_prob)
+
+    output_layer = Dense(vocab_size,
+                         kernel_initializer = tf.truncated_normal_initializer(mean = 0.0, stddev=0.1))
+
+    attn_mech = tf.contrib.seq2seq.BahdanauAttention(rnn_size,
+                                                  enc_output,
+                                                  text_length,
+                                                  normalize=False,
+                                                  name='BahdanauAttention')
+
+    dec_cell = tf.contrib.seq2seq.DynamicAttentionWrapper(dec_cell,
+                                                          attn_mech,
+                                                          rnn_size)
+
+    initial_state = tf.contrib.seq2seq.DynamicAttentionWrapperState(enc_state[0],
+                                                                    _zero_state_tensors(rnn_size,
+                                                                                        batch_size,
+                                                                                        tf.float32))
+    with tf.variable_scope("decode"):
+        training_logits = training_decoding_layer(dec_embed_input,
+                                                  summary_length,
+                                                  dec_cell,
+                                                  initial_state,
+                                                  output_layer,
+                                                  vocab_size,
+                                                  max_summary_length)
+    with tf.variable_scope("decode", reuse=True):
+        inference_logits = inference_decoding_layer(embeddings,
+                                                    vocab_to_int['<GO>'],
+                                                    vocab_to_int['<EOS>'],
+                                                    dec_cell,
+                                                    initial_state,
+                                                    output_layer,
+                                                    max_summary_length,
+                                                    batch_size)
+
+    return training_logits, inference_logits
+
+def seq2seq_model(input_data, target_data, keep_prob, text_length, summary_length, max_summary_length,
+                  vocab_size, rnn_size, num_layers, vocab_to_int, batch_size):
+    '''Use the previous functions to create the training and inference logits'''
+
+    # Use Numberbatch's embeddings and the newly created ones as our embeddings
+    embeddings = word_embedding_matrix
+
+    enc_embed_input = tf.nn.embedding_lookup(embeddings, input_data)
+    enc_output, enc_state = encoding_layer(rnn_size, text_length, num_layers, enc_embed_input, keep_prob)
+
+    dec_input = process_encoding_input(target_data, vocab_to_int, batch_size)
+    dec_embed_input = tf.nn.embedding_lookup(embeddings, dec_input)
+
+    training_logits, inference_logits  = decoding_layer(dec_embed_input,
+                                                        embeddings,
+                                                        enc_output,
+                                                        enc_state,
+                                                        vocab_size,
+                                                        text_length,
+                                                        summary_length,
+                                                        max_summary_length,
+                                                        rnn_size,
+                                                        vocab_to_int,
+                                                        keep_prob,
+                                                        batch_size,
+                                                        num_layers)
+
+    return training_logits, inference_logits
+
+def pad_sentence_batch(sentence_batch):
+    """Pad sentences with <PAD> so that each sentence of a batch has the same length"""
+    max_sentence = max([len(sentence) for sentence in sentence_batch])
+    return [sentence + [vocab_to_int['<PAD>']] * (max_sentence - len(sentence)) for sentence in sentence_batch]
+
+def get_batches(summaries, texts, batch_size):
+    """Batch summaries, texts, and the lengths of their sentences together"""
+    for batch_i in range(0, len(texts)//batch_size):
+        start_i = batch_i * batch_size
+        summaries_batch = summaries[start_i:start_i + batch_size]
+        texts_batch = texts[start_i:start_i + batch_size]
+        pad_summaries_batch = np.array(pad_sentence_batch(summaries_batch))
+        pad_texts_batch = np.array(pad_sentence_batch(texts_batch))
+
+        # Need the lengths for the _lengths parameters
+        pad_summaries_lengths = []
+        for summary in pad_summaries_batch:
+            pad_summaries_lengths.append(len(summary))
+
+        pad_texts_lengths = []
+        for text in pad_texts_batch:
+            pad_texts_lengths.append(len(text))
+
+        yield pad_summaries_batch, pad_texts_batch, pad_summaries_lengths, pad_texts_lengths
