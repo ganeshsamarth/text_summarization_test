24cdf45c50a32f7ff255aedfac4154ec464acea1
ganeshsamarth <47754795+ganeshsamarth@users.noreply.github.com>
2019-07-07 02:17:42 +0530

    Data Manip and data clean have been added
    Data manip is used to essentially work on the data to remove stop words and data clean is being used to convert the words into embeddings

---
+++ data_clean.py
@@ -0,0 +1,217 @@
+from data_manip import *
+import nltk
+nltk.download('stopwords')
+
+def clean_text(text, remove_stopwords = True):
+
+    # Convert words to lower case
+    text = text.lower()
+    #[_\-;%()+&=*%.,!?:$@\[\]/]
+    # Replace contractions with their longer forms
+    if True:
+        text = text.split()
+        new_text = []
+        for word in text:
+            if word in contractions:
+                new_text.append(contractions[word])
+            else:
+                new_text.append(word)
+        text = " ".join(new_text)
+
+    # Format words and remove unwanted characters
+    text = re.sub(r'https?:\/\/.*[\r\n]*', '', text,
+                  flags=re.MULTILINE)
+    text = re.sub(r'\<a href', ' ', text)
+    text = re.sub(r'&amp;', '', text)
+    text = re.sub(r'[_"\-;%()+&=*%.,!?:#$@\[\]/]', '', text)
+    text = re.sub(r'<br />', ' ', text)
+    text = re.sub(r'\'', ' ', text)
+
+    # Optionally, remove stop words
+    if remove_stopwords:
+        text = text.split()
+        stops = set(stopwords.words("english"))
+        text = [w for w in text if not w in stops]
+        text = " ".join(text)
+    return text
+
+clean_summaries = []
+for summary in reviews.Summary:
+    clean_summaries.append(clean_text(summary, remove_stopwords=False))
+print("Summaries are complete.")
+
+clean_texts = []
+for text in reviews.Text:
+    clean_texts.append(clean_text(text))
+print("Texts are complete.")
+
+def count_words(count_dict, text):
+    '''Count the number of occurrences of each word in a set of text'''
+    for sentence in text:
+        for word in sentence.split():
+            if word not in count_dict:
+                count_dict[word] = 1
+            else:
+                count_dict[word] += 1
+word_counts = {}
+
+count_words(word_counts, clean_summaries)
+count_words(word_counts, clean_texts)
+
+print("Size of Vocabulary:", len(word_counts))
+
+embeddings_index = {}
+with open('C:\\Users\\ganeshsamarth\\Downloads\\numberbatch-en-17.06.txt.txt', encoding='utf-8') as f:
+    for line in f:
+        values = line.split(' ')
+        word = values[0]
+        embedding = np.asarray(values[1:], dtype='float32')
+        embeddings_index[word] = embedding
+print('Word embeddings:', len(embeddings_index))
+
+# Find the number of words that are missing from CN, and are used more than our threshold.
+missing_words = 0
+threshold = 20
+
+for word, count in word_counts.items():
+    if count > threshold:
+        if word not in embeddings_index:
+            missing_words += 1
+
+missing_ratio = round(missing_words/len(word_counts),4)*100
+
+print("Number of words missing from CN:", missing_words)
+print("Percent of words that are missing from vocabulary: {}%".format(missing_ratio))
+
+# Limit the vocab that we will use to words that appear ≥ threshold or are in GloVe
+
+#dictionary to convert words to integers
+vocab_to_int = {}
+
+value = 0
+for word, count in word_counts.items():
+    if count >= threshold or word in embeddings_index:
+        vocab_to_int[word] = value
+        value += 1
+
+# Special tokens that will be added to our vocab
+codes = ["<UNK>","<PAD>","<EOS>","<GO>"]
+
+# Add codes to vocab
+for code in codes:
+    vocab_to_int[code] = len(vocab_to_int)
+
+# Dictionary to convert integers to words
+int_to_vocab = {}
+for word, value in vocab_to_int.items():
+    int_to_vocab[value] = word
+
+usage_ratio = round(len(vocab_to_int) / len(word_counts),4)*100
+
+print("Total number of unique words:", len(word_counts))
+print("Number of words we will use:", len(vocab_to_int))
+print("Percent of words we will use: {}%".format(usage_ratio))
+
+# Need to use 300 for embedding dimensions to match CN's vectors.
+embedding_dim = 300
+nb_words = len(vocab_to_int)
+
+# Create matrix with default values of zero
+word_embedding_matrix = np.zeros((nb_words, embedding_dim), dtype=np.float32)
+for word, i in vocab_to_int.items():
+    if word in embeddings_index:
+        word_embedding_matrix[i] = embeddings_index[word]
+    else:
+        # If word not in CN, create a random embedding for it
+        new_embedding = np.array(np.random.uniform(-1.0, 1.0, embedding_dim))
+        embeddings_index[word] = new_embedding
+        word_embedding_matrix[i] = new_embedding
+
+# Check if value matches len(vocab_to_int)
+print(len(word_embedding_matrix))
+
+def convert_to_ints(text, word_count, unk_count, eos=False):
+    '''Convert words in text to an integer.
+       If word is not in vocab_to_int, use UNK's integer.
+       Total the number of words and UNKs.
+       Add EOS token to the end of texts'''
+    ints = []
+    for sentence in text:
+        sentence_ints = []
+        for word in sentence.split():
+            word_count += 1
+            if word in vocab_to_int:
+                sentence_ints.append(vocab_to_int[word])
+            else:
+                sentence_ints.append(vocab_to_int["<UNK>"])
+                unk_count += 1
+        if eos:
+            sentence_ints.append(vocab_to_int["<EOS>"])
+        ints.append(sentence_ints)
+    return ints, word_count, unk_count
+# Apply convert_to_ints to clean_summaries and clean_texts
+word_count = 0
+unk_count = 0
+
+int_summaries, word_count, unk_count = convert_to_ints(clean_summaries, word_count, unk_count)
+int_texts, word_count, unk_count = convert_to_ints(clean_texts, word_count, unk_count, eos=True)
+unk_percent = round(unk_count/word_count,4)*100
+
+print("Total number of words in headlines:", word_count)
+print("Total number of UNKs in headlines:", unk_count)
+print("Percent of words that are UNK: {}%".format(unk_percent))
+
+
+def create_lengths(text):
+    '''Create a data frame of the sentence lengths from a text'''
+    lengths = []
+    for sentence in text:
+        lengths.append(len(sentence))
+
+    return pd.DataFrame(lengths, columns=['counts'])
+
+lengths_summaries = create_lengths(int_summaries)
+lengths_texts = create_lengths(int_texts)
+
+print("Summaries:")
+print(lengths_summaries.describe())
+print()
+print("Texts:")
+print(lengths_texts.describe())
+
+
+def unk_counter(sentence):
+    '''Counts the number of time UNK appears in a sentence.'''
+    unk_count = 0
+    for word in sentence:
+        if word == vocab_to_int["<UNK>"]:
+            unk_count += 1
+    return unk_count
+
+# Sort the summaries and texts by the length of the texts, shortest to longest
+# Limit the length of summaries and texts based on the min and max ranges.
+# Remove reviews that include too many UNKs
+
+sorted_summaries = []
+sorted_texts = []
+max_text_length = 84
+max_summary_length = 13
+min_length = 2
+unk_text_limit = 1
+unk_summary_limit = 0
+
+for length in range(min(lengths_texts.counts), max_text_length):
+    for count, words in enumerate(int_summaries):
+        if (len(int_summaries[count]) >= min_length and
+            len(int_summaries[count]) <= max_summary_length and
+            len(int_texts[count]) >= min_length and
+            unk_counter(int_summaries[count]) <= unk_summary_limit and
+            unk_counter(int_texts[count]) <= unk_text_limit and
+            length == len(int_texts[count])
+           ):
+            sorted_summaries.append(int_summaries[count])
+            sorted_texts.append(int_texts[count])
+
+# Compare lengths to ensure they match
+print(len(sorted_summaries))
+print(len(sorted_texts))
